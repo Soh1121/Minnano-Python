3665327d8a45dd3a892f51d18c72ce4652ea3e38
Soh1121 <satou.shg@gmail.com>
2019-01-26 16:21:28 +0900

    [add]枝分かれのような模様を描くプログラムを追加

---
+++ chapter04/turtle6.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+
+from turtle import *
+
+def branch(length):
+    """
+    長さを引数として受け取って枝を描く
+    """
+    if length < 10:
+        return
+    forward(length)
+    left(30)
+    branch(length/2)
+    right(60)
+    branch(length/2)
+    left(30)
+    forward(-length)
+
+branch(200)
+
+input()
